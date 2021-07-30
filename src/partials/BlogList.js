441e0bfaabd87b89a4c55892c8a0bd87b44add58
taufeeq849 <taufeeq849@gmail.com>
2021-07-30 15:37:40 +0200

    first commit

---
+++ src/partials/BlogList.js
@@ -0,0 +1,83 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+function Instructors() {
+  return (
+    <section>
+      <div className="max-w-6xl mx-auto px-4 sm:px-6">
+        <div className="pb-12 md:pb-20">
+          <div className="lg:flex lg:justify-between">
+            {/* Main content */}
+            <div
+              className="lg:flex-grow"
+              data-aos="fade-down"
+              data-aos-delay="200"
+            >
+              {/* Section title */}Late
+              <h4 className="h3 font-red-hat-display mb-8">
+                Taught by expert instructors
+              </h4>
+              {/* Articles container */}
+              <div className="grid gap-12 sm:grid-cols-2 sm:gap-x-6 md:gap-y-8 items-start">
+                {/* 1st article */}
+                <article className="flex flex-col h-full">
+                  <header>
+                    <Link className="block mb-4" to="/blog-post">
+                      <figure className="relative h-0 pb-9/16">
+                        <img
+                          className="absolute inset-0 w-full h-full object-cover"
+                          src={require("../images/matric-taufeeq.png").default}
+                          width="200"
+                          height="100"
+                          alt="News 01"
+                        />
+                      </figure>
+                    </Link>
+                    <Link className="hover:underline" to="/blog-post">
+                      <h3 className="h4 font-red-hat-display mb-2">
+                        Taufeeq Razak
+                      </h3>
+                    </Link>
+                  </header>
+                  <p className="text-gray-600 dark:text-gray-400 flex-grow">
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
+                    do eiusmod tempor incididunt ut labore et dolore magna
+                    aliqua.
+                  </p>
+                </article>
+
+                {/* 2nd article */}
+                <article className="flex flex-col h-full">
+                  <header>
+                    <Link className="block mb-4" to="/blog-post">
+                      <figure className="relative h-0 pb-9/16">
+                        <img
+                          className="absolute inset-0 w-full h-full object-cover"
+                          src={require("../images/news-02.jpg").default}
+                          width="352"
+                          height="198"
+                          alt="News 02"
+                        />
+                      </figure>
+                    </Link>
+                    <Link className="hover:underline" to="/blog-post">
+                      <h3 className="h4 font-red-hat-display mb-2">
+                        Shreeya Khoosal
+                      </h3>
+                    </Link>
+                  </header>
+                  <p className="text-gray-600 dark:text-gray-400 flex-grow">
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
+                    do eiusmod tempor incididunt ut labore et dolore magna
+                    aliqua.
+                  </p>
+                </article>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+}
+export default Instructors;
